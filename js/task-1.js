b711bfca3420a5c4c07249e2f10bb6b3c53abfec
Bohdan Babych <babychbohdan0@gmail.com>
2024-01-06 23:28:24 +0100

    Homework done

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,163 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits){
-    let totalPrice = quantity * pricePerDroid;
-    if (totalPrice <= customerCredits){
-        return `You ordered ${quantity} droids worth ${totalPrice} credits`
-    }
-    else{
-        return `Insufficient funds`;
-    }
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
+
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// function makeMessage(pizzaName, callback) {
+  
+//   return callback(pizzaName);
+// }
+//   makeMessage("Royal Grand", makePizza);
+// function makePizza(pizzaName, callback) {
+//   console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+//   callback(pizzaName);
+// }
+
+// makePizza("Royal Grand", function deliverPizza(pizzaName) {
+//   console.log(`Delivering pizza ${pizzaName}`);
+// });
+
+// makePizza("Ultracheese", function eatPizza(pizzaName){
+//   console.log(`Eating pizza ${pizzaName}`);
+// });
+// function each(array, callback){
+//   const result = [];
+//   for (const number of array){
+//    const newNumber = callback(number);
+//    result.push(newNumber);
+//   }
+//   return result;
+// };
+
+// console.log(
+//   each( [64,49,36,25,16], function(value){
+//     console.log(value);
+//   return value * 2;
+// })
+// );
+// console.log(
+//   each( [64,49,36,25,16], function(value){
+//     console.log(value);
+//   return value - 10;
+// })
+// );
+// console.log(
+//   each( [64,49,36,25,16], function(value){
+//     console.log(value);
+//   return Math.sqrt(value);
+// })
+// );
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+
+//   orderedItems.forEach(function (number, index){
+//     totalPrice += number;
+//   }
+//   );
+
+//   return totalPrice;
+// }
+// function filterArray(numbers, value) {
+//   let result = [];
+//   numbers.forEach(function(number){
+//     if (value < number){
+//       result.push(number);
+//     } 
+   
+//   })
+//   return result;
+// }
+// function changeEven(numbers, value) {
+//   let newNumbers = [];
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] % 2 === 0) {
+//       newNumbers.push(numbers[i] + value) ;
+//     }
+//     else {newNumbers.push(numbers[i])};
+//   }
+//   return newNumbers;
+// }
+// console.log(changeEven([1, 2, 3, 4, 5], 10));
+// const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+
+// const planetsLengths = planets.map(planet => planet.length);
+// console.log(planetsLengths);
+// const books = [
+//     {
+//       title: "The Last Kingdom",
+//       author: "Bernard Cornwell",
+//       rating: 8.38,
+//     },
+//     {
+//       title: "Beside Still Waters",
+//       author: "Robert Sheckley",
+//       rating: 8.51,
+//     },
+//     {
+//       title: "The Dream of a Ridiculous Man",
+//       author: "Fyodor Dostoevsky",
+//       rating: 7.75,
+//     },
+//     { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//     {
+//       title: "The Dreams in the Witch House",
+//       author: "Howard Lovecraft",
+//       rating: 8.67,
+//     },
+//   ];
+//   const MIN_BOOK_RATING = 8;
+  
+//   const names = books
+  
+//   .filter(book => book.rating > MIN_BOOK_RATING) 
+//   .map(book => book.author)
+//   .toSorted((book1,book2) => book1.localeCompare(book2));
+const getUserNames = (users) => {
+    const allUsersName = users.map(user => user.name);
+
+    return allUsersName;
+
 }
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+console.log(
+    getUserNames([
+    {
+      name: "Moore Hensley",
+      email: "moorehensley@indexia.com",
+      balance: 2811
+    },
+    {
+      name: "Sharlene Bush",
+      email: "sharlenebush@tubesys.com",
+      balance: 3821
+    },
+    {
+      name: "Ross Vazquez",
+      email: "rossvazquez@xinware.com",
+      balance: 3793
+    },
+    {
+      name: "Elma Head",
+      email: "elmahead@omatom.com",
+      balance: 2278
+    },
+    {
+      name: "Carey Barr",
+      email: "careybarr@nurali.com",
+      balance: 3951
+    },
+    {
+      name: "Blackburn Dotson",
+      email: "blackburndotson@furnigeer.com",
+      balance: 1498
+    },
+    {
+      name: "Sheree Anthony",
+      email: "shereeanthony@kog.com",
+      balance: 2764
+    },
+  ])
+  ); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+  
